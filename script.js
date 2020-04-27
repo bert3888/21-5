5185af5cd8083a40a2ba4d2f45d229391ea848a3
laur507p <laur507p@stud.kea.dk>
2020-04-27 01:21:47 +0000

    slide op og ned virker

--- script.js
+++ script.js
@@ -24,25 +24,82 @@
 
   // MENUUUUUUU
 
-  document.querySelector(".mainmenu").addEventListener("click", dropDownBitch)
+
   const dropDown = document.querySelector(".dropdownmenu");
+  const mainMenu = document.querySelector(".mainmenu")
+
+  mainMenu.addEventListener("click", dropDownBitch)
 
 
   function dropDownBitch() {
       console.log("dropdownbitch")
 
-      console.log("dropDown")
 
       if (dropDown.classList.contains("slidedown")) {
-          dropDown.classList.remove("slidedown")
+          dropDown.classList.remove("slidedown");
           dropDown.classList.add("slideop");
 
-      } else {
+
+      } else if (dropDown.classList.contains("slideop")) {
           dropDown.classList.remove("slideop");
           dropDown.classList.add("slidedown")
       }
 
   }
+
+
+
+  //  const boligerne = document.querySelector(".boligerne_mm");
+  //  const destinationer = document.querySelector(".destinationer_mm");
+  //  const konceptet = document.querySelector(".konceptet_mm");
+  //  const okonomi = document.querySelector(".okonomi_mm");
+  //  const omOs = document.querySelector(".omos_mm");
+  //  const kontakt = document.querySelector(".kontakt_mm");
+  //
+  //
+  //  boligerne.addEventListener("click", activeOrNot);
+  //  destinationer.addEventListener("click", activeOrNot);
+  //  konceptet.addEventListener("click", activeOrNot);
+  //  okonomi.addEventListener("click", activeOrNot);
+  //  omOs.addEventListener("click", activeOrNot);
+  //  kontakt.addEventListener("click", activeOrNot);
+  //  const menupunkt = document.querySelector(".menupunkt");
+
+  //  menupunkt.addEventListener("click", activeOrNot);
+
+  function activeOrNot() {
+      console.log("activeornot")
+      //      boligerne.classList.remove("active");
+      //      destinationer.classList.remove("active");
+      //      konceptet.classList.remove("active");
+      //      okonomi.classList.remove("active");
+      //      omOs.classList.remove("active");
+      //      kontakt.classList.remove("active");
+      //
+      //      boligerne.classList.add("inactive");
+      //      destinationer.classList.add("inactive");
+      //      konceptet.classList.add("inactive");
+      //      okonomi.classList.add("inactive");
+      //      omOs.classList.add("inactive");
+      //      kontakt.classList.add("inactive");
+
+      menupunkt.classList.remove("active");
+      menupunkt.classList.add("inactive");
+
+
+      this.classList.remove("inactive")
+      this.classList.add("active");
+
+
+  }
+
+
+
+
+
+
+
+
 
   // Mobile burgermenu
 
